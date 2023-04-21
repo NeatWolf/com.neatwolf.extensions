f45484329b14af513c6c76b738aa108b68906c6b
NeatWolf <alessandro.salvati.github@gmail.com>
2023-04-21 03:22:44 +0100

    Added basic Vector3 to Vector2 component swizzles

--- Runtime/Scripts/Extensions/Vector3Utils.cs
+++ Runtime/Scripts/Extensions/Vector3Utils.cs
@@ -22,5 +22,49 @@ namespace NeatWolf.Extensions
             return vector;
         }
 
+        public static Vector2 xy(this Vector3 vector)
+        {
+            return new Vector2(vector.x, vector.y);
+        }
+        
+        public static Vector2 yx(this Vector3 vector)
+        {
+            return new Vector2(vector.y, vector.x);
+        }
+
+        public static Vector2 xz(this Vector3 vector)
+        {
+            return new Vector2(vector.x, vector.z);
+        }
+
+        public static Vector2 zx(this Vector3 vector)
+        {
+            return new Vector2(vector.z, vector.x);
+        }
+
+        public static Vector2 yz(this Vector3 vector)
+        {
+            return new Vector2(vector.y, vector.z);
+        }
+
+        public static Vector2 zy(this Vector3 vector)
+        {
+            return new Vector2(vector.z, vector.y);
+        }
+
+        public static Vector2 xx(this Vector3 vector)
+        {
+            return Vector2.one * vector.x;
+        }
+        
+        public static Vector2 yy(this Vector3 vector)
+        {
+            return Vector2.one * vector.y;
+        }
+
+        public static Vector2 zz(this Vector3 vector)
+        {
+            return Vector2.one * vector.z;
+        }
     }
 }
